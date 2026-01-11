765579343f044d3becb6740bf14bda1241f7e5ea
Fabian <fgtfabian@gmail.com>
2026-01-11 13:19:09 -0600

    ignore

--- src/config/config.php
+++ src/config/config.php
@@ -11,6 +11,7 @@ define('CONTROLLER_PATH', BASE_PATH . '/src/controllers/');
 define('EMAIL_PATH', BASE_PATH . '/email/');
 define('MODEL_PATH', BASE_PATH . '/src/models/');
 define('PDF_PATH', BASE_PATH . '/pdf/');
+//define('PDF_PATH', BASE_PATH . '../pdf/');
 define('SERVER_PATH', BASE_PATH . '/server/');
 define('UTIL_PATH', BASE_PATH . '/src/utils/');
 define('VIEW_PATH', BASE_PATH . '/src/views/');
